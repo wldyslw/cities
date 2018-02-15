99d01253c86feb37d9db471bea926aeef1ed1765
wldyslw <wldyslw@outlook.com>
2018-02-15 17:35:35 +0300

    environment setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,8 @@
 module.exports = {
     "env": {
         "browser": true,
-        "es6": true
+        "es6": true,
+        'commonjs': true
     },
     "extends": [
         "eslint:recommended",
@@ -22,6 +23,7 @@ module.exports = {
             "error",
             4
         ],
+        "react/prop-types": [0],
         "linebreak-style": [
             "error",
             "unix"
